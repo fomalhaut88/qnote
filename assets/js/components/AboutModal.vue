2af4b8fc7793b9b9fa02a529a1e40a8ed7d4af4c
Alexander <mail@fomalhaut.su>
2020-05-17 15:43:22 +0300

    About

--- assets/js/components/AboutModal.vue
+++ assets/js/components/AboutModal.vue
@@ -1,7 +1,14 @@
 <template>
   <modal ref="refModal">
     <h3>About Qnote</h3>
-    <p>Qnote.</p>
+    <p style="white-space: pre-wrap;">Qnote allows to save text notes in a cloud securely. The main difference to analogues is the thoughtful encryption system, based on elliptic curves, that ensures data privacy even from the owner of the cloud server where the data is stored. The idea is that the application keeps private keys on the client side only and does not send them anywhere, so the server obtains the encrypted information with a digital signature (for the ownership rights). Thus this solution is relevant to those who do not trust the cloud's owner and want to prevent any potential data disclosure, but who want to have all the advantages of the cloud storage. Qnote is quite minimalistic, the source code is available on Github openly, so everybody can learn how it works and make sure why it is reliably.
+
+Qnote is a client side application that interacts by API with the service Hash Storage that keeps the encrypted data with its signatures. Generally Hash Storage is an independent solution to store encrypted data. It can be used by other similar applications as well (even together in the same database), so this is not a part of Qnote.
+
+Links to the source code:
+Qnote: <a href="https://github.com/fomalhaut88/qnote" target="blank">https://github.com/fomalhaut88/qnote</a>
+Hash Storage: <a href="https://github.com/fomalhaut88/hash-storage" target="blank">https://github.com/fomalhaut88/hash-storage</a>
+    </p>
     <button class="btn btn-primary" @click="hide()">OK</button>
   </modal>
 </template>
